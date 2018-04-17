401b6a75cebcc381d1d5625287d173da0c0e5666
eunjiella <tyu5412@gmail.com>
2018-04-17 21:22:12 +0900

    re-upload

---
+++ ex4-2/src/main/java/org/ellalee/ex4_2/MainActivity.java
@@ -0,0 +1,31 @@
+package org.ellalee.ex4_2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btnStartService);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(getApplicationContext(),MyService.class));
+            }
+        });
+        Button btnStop = findViewById(R.id.btnStopService);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(getApplicationContext(),MyService.class));
+            }
+        });
+    }
+}
